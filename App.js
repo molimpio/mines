8539bf5d19ce601fcc5966cda24c303c7bcf9d1c
Moises Olimpio <moiza.olimpio@gmail.com>
2018-10-26 08:21:56 -0300

    Component Field 02

--- App.js
+++ App.js
@@ -10,7 +10,12 @@ export default class App extends Component {
         <Text style={styles.welcome}>Iniciando o Mines!</Text>        
         <Text style={styles.instructions}>Tamanho da grade:
             {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-        <Field />            
+        <Field />
+        <Field opened />            
+        <Field opened nearMines={1} />            
+        <Field opened nearMines={2} />            
+        <Field opened nearMines={3} />            
+        <Field opened nearMines={6} />            
       </View>
     );
   }
